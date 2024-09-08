72368da35b3712b83be85740cf726804eb4149f1
Leonid Konstantinov <leanidkonstantinov@yandex.ru>
2024-09-08 15:35:25 +0300

    new

--- types.py
+++ types.py
@@ -1,2 +1,8 @@
-first_num : int = 5
-print(first_num)
+import requests
+
+api_url = 'http://api.open-notify.org/iss-now.json'
+response = requests.get(api_url)
+if response.status_code == 200:
+    print(response.text)
+else:
+    print(response.status_code)
