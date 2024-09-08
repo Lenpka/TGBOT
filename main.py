730711f1bdc98fa6055528d0f9263bffe04e00cf
Leonid Konstantinov <leanidkonstantinov@yandex.ru>
2024-09-08 20:06:36 +0300

    for papa

---
+++ main.py
@@ -0,0 +1,36 @@
+#Импорт библиотек
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+BOT_TOKEN = '7392036746:AAHQQ1PS27ZxY1NvsfPMPR0h0oy6R4Y2fBQ'
+
+# Создаем объекты бота и диспетчера
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+
+# Этот хэндлер будет срабатывать на команду "/start"
+@dp.message(Command(commands=["start"]))
+async def process_start_command(message: Message):
+    await message.answer('Напиши мне что-нибудь')
+
+
+# Этот хэндлер будет срабатывать на команду "/help"
+@dp.message(Command(commands=['help']))
+async def process_help_command(message: Message):
+    await message.answer(
+        'Напиши мне что-нибудь и в ответ '
+        'я пришлю тебе твое сообщение'
+    )
+
+
+# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
+# кроме команд "/start" и "/help"
+@dp.message()
+async def send_echo(message: Message):
+    await message.reply(text=message.text)
+
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
