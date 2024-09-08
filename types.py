f297729bce533e84b212839d9f11cc5bd1b470bb
Leonid Konstantinov <leanidkonstantinov@yandex.ru>
2024-09-08 15:55:57 +0300

    deleted

--- types.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-api_url = 'http://api.open-notify.org/iss-now.json'
-response = requests.get(api_url)
-if response.status_code == 200:
-    print(response.text)
-else:
-    print(response.status_code)
