90c8536f561e4dc557a005ee07843e8973ac34f5
Leonid Konstantinov <leanidkonstantinov@yandex.ru>
2024-09-15 21:18:54 +0300

    dict

---
+++ schedule.py
@@ -0,0 +1,9 @@
+FNM_first_course = dict(
+    "Monday" : [English, Seminar_Chemistry, Lunch, Practice_Chemistry, Practice_Chemistry]
+    "Tuesday" : [Math_Seminar, Chemistry_Lection, Practice_Chemistry, Lunch, Seminar_Chemisty]
+    "Wednesday" : [Programming_and_computers, Programming_and_computers, Lunch, Phisical_Culture, Laboratory, Laboratory]
+    "Thursday" : [Math_Seminar, Math_Lecture, Math_Lecture, English]
+    "Friday" : [Chemisry_Lecture, Lunch, Intoduction_in_specialty, Seminar_Chemisry]
+    "Saturday" : [Lineal_agebra, Lineal_algebra, Lunch, Phisical_Culture]
+    "Sunday" : [Nothing]
+)
