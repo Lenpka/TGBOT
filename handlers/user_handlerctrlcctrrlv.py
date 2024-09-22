607cd200789deb347b5edbd5f68eba2171d70430
jskonst <jskonst@yandex.ru>
2024-09-22 20:07:49 +0300

    cleanup for secrets

--- handlers/user_handlerctrlcctrrlv.py
+++ handlers/user_handlerctrlcctrrlv.py
@@ -2,7 +2,7 @@ from aiogram import Router, F, Bot
 from aiogram.filters import Command, CommandStart
 from aiogram.types import Message
 
-from handlers.schedule_get import schedule_test_get_data
+from handlers.schedule_get import schedule_test_get_data, homework_get_data
 from lexicon_ru import LEXICON_COMMAND_RU
 from keyboards import get_command_menu
 import datetime
@@ -10,12 +10,16 @@ import datetime
 
 router = Router()
 schedule = schedule_test_get_data()
+homework = homework_get_data()
 
 def getDay(day: datetime.datetime) -> str:
     day = day.strftime("%A")
     lectures = schedule[day]
     return '\n'.join([str(lecture) for lecture in lectures])
-
+def getWork(day: datetime.datetime) -> str:
+    day = day.strftime("%A")
+    work = homework[day]
+    return '\n'.join([str(ork) for ork in work])
 @router.message(Command(commands=['scheduletoday']))
 async def process_start_command(message: Message):
     today = datetime.datetime.now()
@@ -34,8 +38,20 @@ async def tommorow_command(message: Message):
     tommorow = datetime.datetime.now() + datetime.timedelta(days=1)
     res = getDay(tommorow)
     await message.answer(res)
+@router.message(Command(commands=['scheduleaftertommorow']))
+async def tommorow_command(message: Message):
+    tommorow = datetime.datetime.now() + datetime.timedelta(days=2)
+    res = getDay(tommorow)
+    await message.answer(res)
 
 @router.message(Command(commands=['homework']))
 async def process_setmenu_command(message: Message, bot: Bot):
-    await bot.set_my_commands(get_command_menu())
-    await message.answer(text=LEXICON_COMMAND_RU['/homework'])
+    tommorow = datetime.datetime.now() + datetime.timedelta(days=1)
+    res = getWork(tommorow)
+    await message.answer(res)
+
+@router.message(Command(commands=['homeworknotnow']))
+async def process_setmenu_command(message: Message, bot: Bot):
+    tommorow = datetime.datetime.now() + datetime.timedelta(days=2)
+    res = getWork(tommorow)
+    await message.answer(res)
