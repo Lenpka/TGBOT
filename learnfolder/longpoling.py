4c28e4306bfc7d17c59207ec192a1fee7aebcdf6
Lenpka <leanidkonstantinov@yandex.ru>
2025-08-28 17:22:57 +0300

    with timeout delay

--- learnfolder/longpoling.py
+++ learnfolder/longpoling.py
@@ -2,30 +2,29 @@ import requests
 import time
 
 BOT_URL = "https://api.telegram.org/bot"
-
+BOT_TOKEN  = "7392036746:AAHofH1Aexm8azr9oCmgtXbVm-qZ593Jpno"
 
 chat_id:int
 text = "Something"
-timesToStop = 10
+timesToStop = 30
 offset = -2
 
 
 while True:
     wait_for_response = time.time()#0
-    startTime = time.time()
-    while (abs(wait_for_response - startTime)) <= timesToStop:
+
         #0
-        updates = requests.get(f'{BOT_URL}{BOT_TOKEN}/getUpdates?offset={offset +1}').json()
+    updates = requests.get(f'{BOT_URL}{BOT_TOKEN}/getUpdates?offset={offset +1}&timeout={timesToStop}').json()
        
-        if updates['result']:
-            for reques in updates['result']:
-                offset = reques['update_id']
-                chat_id = reques['message']['from']['id']
-                wait_for_response = time.time()
-                requests.get(f'{BOT_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={text}')
-                
-                print ('БЫЛО ОБНОВЛЕНИЕ')
-        time.sleep(1)
-        print(f"Время = {wait_for_response- time.time()}")
-    wait_for_response = time.time()#0
-    startTime = time.time()
+    if updates['result']:
+        for reques in updates['result']:
+            offset = reques['update_id']
+            chat_id = reques['message']['from']['id']
+
+            requests.get(f'{BOT_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={text}')
+            
+            print ('БЫЛО ОБНОВЛЕНИЕ')
+    time.sleep(1)
+    endTime = time.time()
+    print(f"Время = {wait_for_response- endTime}")#0
+
