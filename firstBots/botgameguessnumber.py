4adb5b67106497a89166befb842afc574fb64ee2
Lenpka <leanidkonstantinov@yandex.ru>
2025-08-29 19:43:44 +0300

    with state dictionary bot

--- firstBots/botgameguessnumber.py
+++ firstBots/botgameguessnumber.py
@@ -11,6 +11,15 @@ BOT_TOKEN = os.getenv("BOT_TOKEN") # Важно локально указать 
 bot = Bot(token=BOT_TOKEN)
 disp = Dispatcher()
 
+
+user_state = {
+    "Is_Active": 0,
+    "Count":0,
+    "attempts" : 0
+
+
+}
+
 def getRandom() ->int:
     value = random.randint(1, 100)
     return value
@@ -19,22 +28,30 @@ value = getRandom()
 async def startmessage(message:Message) -> int:
     print(f'ТССССССССССССС, ТИХО, СЕКРЕТНОЕ ЗНАЧЕНИЕ VALUE = {value}')
     await message.answer("ДАВАЙ ИГРАТЬ, ДАВАЙ натуральное ЧИСЛО ОТ 1 до 100 угадаем")
-
+    user_state["Is_Active"] = 1
+print(user_state["Is_Active"])
 
 @disp.message(lambda x: x.text and x.text.isdigit() and int(x.text) == value)
 async def youGuess(message:Message):
     await message.answer ("МОИ ПОЗДРАВЛЕНИЕ, В ТОЧКУ, ЕЩЕ РАЗ? Я ЗАГАДАЛ!")
     global value
     value = getRandom()
+    if user_state["Is_Active"] == 1:
+
+        user_state["Count"] += 1
 
 @disp.message (lambda x: x.text and x.text.isdigit() and int(x.text) < value)
 async def someBigger(message:Message):
     await message.answer("НЕМНОГО НЕДОБОР")
+    if user_state["Is_Active"] == 1:
+        user_state['attempts']+=1 
 
 
 @disp.message (lambda x: x.text and x.text.isdigit() and int(x.text) > value)
 async def someBigger(message:Message):
     await message.answer("НЕМНОГО ПЕРЕБОР")
+    if user_state["Is_Active"] == 1:
+        user_state['attempts'] += 1
 
 @disp.message(Command(commands = ("help")))
 async def sendHelp(message:Message):
@@ -42,7 +59,12 @@ async def sendHelp(message:Message):
 @disp.message(Command(commands = ("end")))
 async def sendHelp(message:Message):
     await message.answer("Было приятно играть!")
-
+    user_state["attempts"] = 0
+    user_state["Count"] = 0
+    user_state["Is_Active"]= 0
+@disp.message(Command(commands = ("stat")))
+async def sendHelp(message:Message):
+    await message.answer(f"Твоя точность = {user_state['Count']/user_state['attempts']}")
 
 @disp.message()
 async def iDontUnderstand(message:Message):
