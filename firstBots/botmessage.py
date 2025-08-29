fc2281912e55d9933410e762fd17da52e8ea6e69
Lenpka <leanidkonstantinov@yandex.ru>
2025-08-29 08:54:19 +0300

    Photo and register

--- firstBots/botmessage.py
+++ firstBots/botmessage.py
@@ -1,27 +1,42 @@
 from aiogram import Bot, Dispatcher # API и маршрутизация апдейтов
 from aiogram.filters import Command # Хэндлер команд, то есть обработка начинающихся с / команд
-from aiogram.types import Message
+from aiogram.types import Message, ContentType
 import os
 from dotenv import load_dotenv
+#add photo handler
+from aiogram import F # Магический фильтр
+
 
 BOT_TOKEN = os.getenv("BOT_TOKEN") # Важно локально указать переменную
 
 bot = Bot(token = BOT_TOKEN)
 dispatcher = Dispatcher()
 
-@dispatcher.message(Command(commands="start"))
+
 async def message_send(message: Message):
     print("Сообщение получил")
     await message.answer("Привет, это воскрешение телеграм бота")
 
-@dispatcher.message(Command(commands="help"))
+
 async def help_info(message:Message):
     await message.answer(text = "Выбери опцию ниже по навигации: \n если их мало, пиши сюда:")
+
+async def photo_get(message:Message ):
+    await message.answer("Классная фотка, держи обратно! \n")
+    await message.reply_photo(message.photo[0].file_id)
+
+
+
 # Если не start и не help
-@dispatcher.message()
+
 async def if_not_list(message:Message):
     await message.answer("Извини не понимаю")
     await message.answer_photo(photo = "https://opencartsuppliers.com/ru/bl-content/uploads/media/product-photo9.png")
 
+#Регистрация методов
+dispatcher.message.register(message_send, Command(commands = "start"))
+dispatcher.message.register(help_info, Command(commands = "help"))
+dispatcher.message.register(photo_get, F.photo)
+dispatcher.message.register(if_not_list)
 if __name__ == "__main__": # ЕСЛИ ИСПОЛНЯЕМЫЙ
     dispatcher.run_polling(bot)
