61f94b0fa3af4b109ec15a487b3568079703cb67
Lenpka <leanidkonstantinov@yandex.ru>
2025-08-29 21:39:41 +0300

    Multiusers and states

--- firstBots/botgameguessnumber.py
+++ firstBots/botgameguessnumber.py
@@ -2,7 +2,7 @@ from aiogram.types import Message
 import time
 import random
 from aiogram import Bot, Dispatcher
-from aiogram.filters import Command
+from aiogram.filters import Command, CommandStart
 from aiogram import F
 import os
 BOT_TOKEN:str
@@ -10,15 +10,9 @@ BOT_TOKEN = os.getenv("BOT_TOKEN") # Важно локально указать 
 
 bot = Bot(token=BOT_TOKEN)
 disp = Dispatcher()
+users = {}
 
 
-user_state = {
-    "Is_Active": 0,
-    "Count":0,
-    "attempts" : 0
-
-
-}
 
 def getRandom() ->int:
     value = random.randint(1, 100)
@@ -28,30 +22,39 @@ value = getRandom()
 async def startmessage(message:Message) -> int:
     print(f'ТССССССССССССС, ТИХО, СЕКРЕТНОЕ ЗНАЧЕНИЕ VALUE = {value}')
     await message.answer("ДАВАЙ ИГРАТЬ, ДАВАЙ натуральное ЧИСЛО ОТ 1 до 100 угадаем")
-    user_state["Is_Active"] = 1
-print(user_state["Is_Active"])
+    if message.from_user.id not in users:
+      
+        users[message.from_user.id] ={
+    "Is_Active": 0,
+    "Count":0,
+    "attempts" : 0
+}
+    users[message.from_user.id]["Is_Active"] = 1
+    print(users[message.from_user.id]["Is_Active"])
 
 @disp.message(lambda x: x.text and x.text.isdigit() and int(x.text) == value)
 async def youGuess(message:Message):
-    await message.answer ("МОИ ПОЗДРАВЛЕНИЕ, В ТОЧКУ, ЕЩЕ РАЗ? Я ЗАГАДАЛ!")
-    global value
-    value = getRandom()
-    if user_state["Is_Active"] == 1:
+    if users[message.from_user.id]["Is_Active"] == 1:
+        await message.answer ("МОИ ПОЗДРАВЛЕНИЕ, В ТОЧКУ, ЕЩЕ РАЗ? Я ЗАГАДАЛ!")
+        users[message.from_user.id]["Count"] += 1
+        global value
+        value = getRandom()
 
-        user_state["Count"] += 1
+
+        
 
 @disp.message (lambda x: x.text and x.text.isdigit() and int(x.text) < value)
 async def someBigger(message:Message):
     await message.answer("НЕМНОГО НЕДОБОР")
-    if user_state["Is_Active"] == 1:
-        user_state['attempts']+=1 
+    if users[message.from_user.id]["Is_Active"] == 1:
+        users[message.from_user.id]['attempts']+=1 
 
 
 @disp.message (lambda x: x.text and x.text.isdigit() and int(x.text) > value)
 async def someBigger(message:Message):
     await message.answer("НЕМНОГО ПЕРЕБОР")
-    if user_state["Is_Active"] == 1:
-        user_state['attempts'] += 1
+    if users[message.from_user.id]["Is_Active"] == 1:
+        users[message.from_user.id]['attempts'] += 1
 
 @disp.message(Command(commands = ("help")))
 async def sendHelp(message:Message):
@@ -59,12 +62,15 @@ async def sendHelp(message:Message):
 @disp.message(Command(commands = ("end")))
 async def sendHelp(message:Message):
     await message.answer("Было приятно играть!")
-    user_state["attempts"] = 0
-    user_state["Count"] = 0
-    user_state["Is_Active"]= 0
+    users[message.from_user.id]["attempts"] = 0
+    users[message.from_user.id]["Count"] = 0
+    users[message.from_user.id]["Is_Active"]= 0
 @disp.message(Command(commands = ("stat")))
 async def sendHelp(message:Message):
-    await message.answer(f"Твоя точность = {user_state['Count']/user_state['attempts']}")
+    if users[message.from_user.id]['Count'] > 0:
+        await message.answer(f"Твоя точность = {users[message.from_user.id]['Count']*100/(users[message.from_user.id]['attempts']+users[message.from_user.id]['Count'])}%")
+    else:
+        await message.answer ("Угадай хоть 1 число)")
 
 @disp.message()
 async def iDontUnderstand(message:Message):
